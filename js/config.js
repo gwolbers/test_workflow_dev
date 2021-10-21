ddb9db2dff471954e12197253029b5be9fd86f73
gwolbers <>
2021-10-21 12:20:16 +0000

    Update from https://github.com/gwolbers/test_workflow/commit/f3ed214d2d3e1670066425f97f4bea77823c5948

---
+++ js/config.js
@@ -0,0 +1,29 @@
+var respecConfig =
+{
+  pubDomain: "ow",
+  specStatus: "GN-DV",
+  specType: "ST",
+  license: "cc-by-nd",
+  publishDate: "2021-09-08",
+  previousPublishDate: "2021-07-05",
+  previousMaturity: "GN-DV",
+  authors: [
+    {
+      name: "Richard de Graaf",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  shortName: "InformatiemodelOmgevingswetImow",
+  format: "markdown",
+  github: "https://github.com/Geonovum/ow-InformatiemodelOmgevingswetImow",
+  issueBase: "https://github.com/Geonovum/ow-InformatiemodelOmgevingswetImow/issues",
+  edDraftURI: "https://Geonovum.github.io/ow-InformatiemodelOmgevingswetImow"
+}
